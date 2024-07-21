fa1a2ec66a60aadb4b27fe3b5528eec240f61958
deianapatrut <deianapatrut@yahoo.com>
2024-07-21 19:05:05 +0300

    add error calculations

--- DGA/main.py
+++ DGA/main.py
@@ -6,8 +6,12 @@ from imageReaders.DrivfaceInput import DrivfaceInput
 from backbone.clustering import clustering
 from backbone.my_pipeline import my_pipeline
 import os
+import csv
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
 
 from backbone.processor import Processor
 
@@ -75,8 +79,72 @@ def f3(imgsrc):
     print(proc.get_action_list())
     print()
     
+ 
+def plot_gaze_errors():
+        parent_dir = os.path.dirname(r'C:\Users\dpatrut\source\repos\Driver-Gaze-Analizer\DGA\results\trying\gaze_estimations.csv')
+        with open(r'C:\Users\dpatrut\source\repos\Driver-Gaze-Analizer\DGA\results\trying\gaze_estimations.csv', 'r') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            data = {rows[0]: rows[1:] for rows in csv_reader}
+        pitches = np.array(list(map(float, data['Original Pitch'])))
+        yaws = np.array(list(map(float, data['Original Yaw'])))
+        system_pitches = np.array(list(map(float, data['System Pitch'])))
+        system_yaws = np.array(list(map(float, data['System Yaw'])))
+        frames = list(range(len(pitches)))
+        
+        with open(r'C:\Users\dpatrut\source\repos\Driver-Gaze-Analizer\DGA\results\trying\pitch_and_yaw.csv', 'r') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            data = {rows[0]: rows[1:] for rows in csv_reader}
+        pf_pitches = np.array(list(map(float, data['PF Pitch'])))
+        pf_yaws = np.array(list(map(float, data['PF Yaw'])))
+        
+        with open(r'C:\Users\dpatrut\source\repos\Driver-Gaze-Analizer\DGA\results\trying\gt.csv', 'r') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            data = {rows[0]: rows[1:] for rows in csv_reader}
+        gt_pitches = np.array(list(map(float, data['Ground Truth Pitch'])))
+        gt_yaws = np.array(list(map(float, data['Ground Truth Yaw'])))
+        
+        mse_pitch_original = mean_squared_error(gt_pitches, pitches)
+        mse_yaw_original = mean_squared_error(gt_yaws, yaws)
+        
+        mse_pitch_system = mean_squared_error(gt_pitches, system_pitches)
+        mse_yaw_system = mean_squared_error(gt_yaws, system_yaws)
+        
+        mse_pitch_pf = mean_squared_error(gt_pitches, pf_pitches)
+        mse_yaw_pf = mean_squared_error(gt_yaws, pf_yaws)
+        
+        # Calculate absolute error for each frame
+        asolute_errors_pitch_original = abs(gt_pitches - pitches)
+        asolute_errors_yaw_original = abs(gt_yaws - yaws)
     
-    
+        asolute_errors_pitch_system = abs(gt_pitches - system_pitches)
+        asolute_errors_yaw_system = abs(gt_yaws - system_yaws)
+        
+        asolute_errors_pitch_pf = abs(gt_pitches - pf_pitches)
+        asolute_errors_yaw_pf = abs(gt_yaws - pf_yaws)
+
+        # Plot MSE for Pitch Original
+        plt.subplot(1, 3, 1)
+        plt.bar(frames, asolute_errors_pitch_original, color='blue', label='Original')
+        plt.xlabel('Frame')
+        plt.ylabel('Absolute error')
+        plt.legend()
+        plt.grid(axis='y')
+        # Plot MSE for Pitch System
+        plt.subplot(1, 3, 2)
+        plt.bar(frames, asolute_errors_pitch_system, color='green', alpha=0.6, label='System')
+        plt.xlabel('Frame')
+        plt.ylabel('Absolute error')
+        plt.legend()
+        plt.grid(axis='y')
+        # Plot MSE for Pitch PF
+        plt.subplot(1, 3, 3)
+        plt.bar(frames, asolute_errors_pitch_pf, color='red', alpha=0.6, label='PF')
+        plt.xlabel('Frame')
+        plt.ylabel('Absolute error')
+        plt.legend()
+        plt.grid(axis='y')
+        plt.tight_layout()
+        plt.show() 
 
 def main():
     
@@ -87,7 +155,11 @@ def main():
     imgsrc = DrivfaceInput((1,2,3,4))
     proc = gproc()
     #proc.render(imgsrc)
-    proc.render(imgsrc, savePath=r'C:\Users\dpatrut\source\repos\Driver-Gaze-Analizer\DGA\results\trying\image_{imNr}.png')
+    #proc.render(imgsrc, savePath=r'C:\Users\dpatrut\source\repos\Driver-Gaze-Analizer\DGA\results\trying\image_{imNr}.png')
+    #proc.saveResults(imgsrc, savePath=r'C:\Users\dpatrut\source\repos\Driver-Gaze-Analizer\DGA\results\trying\gaze_estimations.csv')
+    #proc.plot_gaze_estimations_from_csv(r'C:\Users\dpatrut\source\repos\Driver-Gaze-Analizer\DGA\results\trying\gaze_estimations.csv')
+    
+    plot_gaze_errors()
     
     #f4(imgsrc)
 
