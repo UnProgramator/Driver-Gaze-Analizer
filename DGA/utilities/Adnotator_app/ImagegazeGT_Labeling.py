2bcce5318f30b6939712dfa28f5da45da11c73cd
Pescaru Alexandru-Mihai <apescaru11@gmail.com>
2025-08-04 15:46:22 +0300

    Added posibility to mark frames as disturbance present (1 or corect 0) and a few qol improvments

--- DGA/utilities/Adnotator_app/ImagegazeGT_Labeling.py
+++ DGA/utilities/Adnotator_app/ImagegazeGT_Labeling.py
@@ -1,26 +1,28 @@
 from operator import contains
-from typing import Any, Final
+from typing import Any, Final, override
 import cv2
 from l2cs.results import GazeResultContainer
 import numpy as np
 import random
 import tkinter.filedialog
+import tkinter.messagebox as tkmb
 
 import pandas
+import os
 
 from backbone import my_pipeline
 
 
 # Parameters
 
-batch = 4 # 1-4
+batch = 1 # 1-4
 
 basePath = 'D:/DCIM/images_12fps/'
 
-inTestFile = basePath+'pitch_and_yaw_b{}.csv'.format(batch)
-gtTestFile = basePath+'gt_b{}.csv'.format(batch)
-imgDir     = basePath+'batch{}/'.format(batch)
-gtWndName:Final[str] = 'Ground Truth'
+inFilePath:Final[str] = basePath+f'pitch_and_yaw_b{batch}.csv'
+gtFilePath:Final[str] = basePath+f'gt_b{batch}.csv'
+imgDir:Final[str]     = basePath+f'batch{batch}/'
+gtWndName:Final[str]  = 'Ground Truth'
 imgWndName:Final[str] = 'Current Farme'
 
 
@@ -38,33 +40,41 @@ crtFrame = 0    #curent frame to be processed
 
 pv   = 0        # current point in vieport, in 0 .. N-1
 
-color1:Final = (0, 102, 204)   # graph color
+#opencv BGR (Blue, Green, Red)
+colRed:Final=(125, 0, 250)
+color1:Final = (0, 175, 250)   # graph color
+color2:Final = color1   # point color
+color3:Final = (255, 20, 20)   # gt graph color
+color4:Final = color3   # gt point color
+
 thickness1:Final = 1  # graph line tickness
 
 radius2:Final = 3     # point radius
-color2:Final = (0, 179, 255)   # point color
+
 thickness2:Final = 2  # point line tickness
 
-color3:Final = (128, 128, 0)   # gt graph color
+
 thickness3:Final = 1  # gt graph line tickness
 
 radius4:Final = 5     # gt point radius
-color4:Final = (255, 20, 20)   # gt point color
+
 thickness4:Final = 2  # gt point line tickness
 
 radius5:Final = 9     # crt point radius
 color5:Final = (0, 0, 255)   # crt point color
 thickness5:Final = 2  # crt point line tickness
 
-pitch:list[str]
-yaw:list[str]
+pitch:list[float]
+yaw:list[float]
 
-gtp:list[str]
-gty:list[str]
+gtp:list[float]
+gty:list[float]
+ber:list[int]
 
 names:list[str]
 
-bd:np.ndarray[Any,np.dtype[np.float32]]|None = None
+bd   :np.ndarray[Any,np.dtype[np.float32]]|None = None
+score:np.ndarray[Any,np.dtype[np.string_]]|None=None
 
 def main():
     global totalFrames, pcrt, pv,pitch,yaw,gtp,gty
@@ -74,26 +84,18 @@ def main():
     print('Gaze labeling tool v0.2 started ...')
 
     # reading csw file
-    inFileName:str
-    gtFileName:str
-    (inFileName,gtFileName) = getFiles()
+    # inFileName:str
+    # gtFileName:str
+    # (inFileName,gtFileName) = getFiles()
 
     #reads a fucking csv file, cahnge to use py-csv
-    print('Extracting pitch and yaw values from '+inFileName+'.')
+    # print('Extracting pitch and yaw values from '+inFileName+'.')
     
     print('read l2cs output from file')
-    pitch,yaw = readInFile(inFileName,True)
+    readInFile()
 
-    print('read gt from file')
-    try:
-        gtp,gty=readGt(gtFileName,True)
-    except:
-        gtp = pitch.copy()     # Ground Truth point list
-        gty = yaw.copy()
-
-    if len(gtp) == 0:
-        gtp = pitch.copy()     # Ground Truth point list
-        gty = yaw.copy()
+    print('atempting to read gt from file')
+    readGt()
 
     #diplay window graph
     cv2.namedWindow(gtWndName, cv2.WINDOW_NORMAL)
@@ -110,7 +112,7 @@ def main():
         k = cv2.waitKeyEx(0)
 
         if k == 27: # esc
-            saveGt(gtFileName)
+            saveGt()
             cv2.destroyAllWindows()
             print('Finish')
             exit(0)
@@ -142,18 +144,18 @@ def main():
             #if (DEBUG): print('up')
             if isPitch:
                 if float(gtp[crtFrame])<VMAX:
-                    gtp[crtFrame] = str(float(gtp[crtFrame]) + delta + random.random()/500)
+                    gtp[crtFrame] = gtp[crtFrame] + delta + random.random()/500
             else:
                 if float(gty[crtFrame])<VMAX:
-                    gty[crtFrame] = str(float(gty[crtFrame]) + delta + random.random()/500)
+                    gty[crtFrame] = gty[crtFrame] + delta + random.random()/500
         elif k== 2621440 or k == ord('s'):
             #if (DEBUG): print('down')
             if isPitch:
                 if float(gtp[crtFrame])>-VMAX:
-                    gtp[crtFrame] = str(float(gtp[crtFrame]) - delta - random.random()/500)
+                    gtp[crtFrame] = gtp[crtFrame] - delta - random.random()/500
             else:
                 if float(gty[crtFrame])>-VMAX:
-                    gty[crtFrame] = str(float(gty[crtFrame]) - delta - random.random()/500)
+                    gty[crtFrame] = gty[crtFrame] - delta - random.random()/500
         
         elif k == ord('q'):
             if crtFrame > 0:
@@ -172,9 +174,9 @@ def main():
                 print('Error, can\'t copy the state of previouse frame for frame 0');
 
         elif k == ord('y'):
-            saveGt(gtFileName)
+            saveGt(False)
 
-        elif k == ord('c'):             # c
+        elif k == ord('c'):
             print('Switching to {}!'.format('Pitch' if isPitch else 'Yaw'))
             isPitch = not isPitch  
 
@@ -186,6 +188,9 @@ def main():
             else:
                 gty[crtFrame] = yaw[crtFrame]
                 print('reset yaw')
+        
+        elif k == ord('x'):
+            ber[crtFrame] ^= 1 # = int(not ber[crtFrame]) # sau exlusiv schimba ultimul bit
 
         elif k == ord('o'):
             isPitch = True
@@ -214,97 +219,65 @@ def main():
                 crtFrame=totalFrames-1
             pv = N-1
 
-
+        
         if isPitch:
             display(gtWndName, imgWndName, bg, pitch, gtp, '  -  Pitch')
         else:
             display(gtWndName, imgWndName, bg, yaw, gty, '  -  Yaw')
 
-def saveGt(gtFilePath:str):
-    global gtp, gty
+def readGt():
+    global gtp, gty, ber, gtFilePath
+    if not os.path.exists(gtFilePath):
+        print('No GT file was found')
+        gtp = pitch.copy()
+        gty = yaw.copy()
+        ber = [0 for _ in range(len(gtp))]
+    else:
+        pd = pandas.read_csv(gtFilePath)
+        gtp = pd['Ground Truth Pitch'].tolist()
+        gty = pd['Ground Truth Yaw'].tolist()
+        ber = pd['Frame Err'].tolist() if 'Frame Err' in pd else [0 for _ in range(len(gtp))]
+        print('Succesfully red GT from file')
+
+def saveGt(askSave:bool=True):
+    if askSave:
+        a = tkmb.askquestion('Save changes', 'Do you want  to save the changes?')
+        if a == 'no':
+            return;
+
+    global gtp, gty, ber, gtFilePath
     with open(gtFilePath,'w+') as fid:
-        print('Ground Truth Pitch,Ground Truth Yaw',file=fid)
+        print('Ground Truth Pitch,Ground Truth Yaw,Frame Err',file=fid)
         for i in range(len(gtp)):
             point1 = gtp[i]
             point2 = gty[i]
-            print(point1,point2,file=fid,sep=',')
+            er=ber[i]
+            print(point1,point2,er,file=fid,sep=',')
     print(f'Ground Truth saved in {gtFilePath} !')
 
-def readInFile(inFileName:str,normalCsv:bool=False) -> tuple[list[str],list[str]]:
-    pitch:list[str]
-    yaw:list[str]
-    global totalFrames, names, bd
-    if not normalCsv:
-        with open(inFileName,'r') as inFile:
-            line = inFile.readline().rstrip()
-            pitch = line.split(',')
-            pitch = pitch[1:]
-            
-            print(totalFrames, 'frames - pitch: ', pitch)
-            line = inFile.readline().rstrip()
-            yaw = line.split(',')
-            yaw = yaw[1:]
-            assert len(yaw)==totalFrames, 'Invalid number of YAW values. (<> with PITCH values)!'    
-            print(totalFrames, 'frames - yaw: ', yaw)
-    else:
-        pd = pandas.read_csv(inFileName,dtype='str')
-        pitch = pd['Pitch']
-        yaw = pd['Yaw']
-        names = [i.strip() for i in pd['FileName']]
-        if contains(pd,'B1'):
+def readInFile() -> tuple[list[str],list[str]]:
+    global totalFrames, names, bd, score, pitch, yaw
 
-            B1:list[float] = [float(i) for i in pd['B1']]
-            B2:list[float] = [float(i) for i in pd['B2']]
-            B3:list[float] = [float(i) for i in pd['B3']]
-            B4:list[float] = [float(i) for i in pd['B4']]
-            bd = np.array([[B1[i],B2[i],B3[i],B4[i]] for i in range(len(B1))], dtype=np.float32)
+    pd = pandas.read_csv(inFilePath)
+    pitch = pd['Pitch'].tolist()
+    yaw = pd['Yaw'].tolist()
+    names = [i.strip() for i in pd['FileName']]
+    if 'B1' in pd:
+        B1:list[float] = [float(i) for i in pd['B1']]
+        B2:list[float] = [float(i) for i in pd['B2']]
+        B3:list[float] = [float(i) for i in pd['B3']]
+        B4:list[float] = [float(i) for i in pd['B4']]
+        bd = np.array([[B1[i],B2[i],B3[i],B4[i]] for i in range(len(B1))], dtype=np.float32)
+
+    if 'score' in pd:
+        score=pd['score']
     
     totalFrames = len(pitch)
     assert len(yaw)==totalFrames, 'Invalid number of YAW values. (<> with PITCH values)!'   
     print('number of frames',totalFrames)
-    return pitch,yaw
-
-def readGt(gtFileName:str,normalCsv:bool=False) -> tuple[list[str],list[str]]:
-    gtp:list[str]
-    gty:list[str]
-    if not normalCsv:
-        #open gtFile (if exists)
-        gtFile = open(gtFileName,'r')
-        line = gtFile.readline().rstrip()
-        gtp = line.split(',')
-        gtp = gtp[1:]
-        line = gtFile.readline().rstrip()
-        gtFile.close()
-        gty = line.split(',')
-        gty = gty[1:] 
-    
-        return gtp,gty
-    else:
-        pd = pandas.read_csv(gtFileName,dtype='str')
-        gtp = pd['Ground Truth Pitch']
-        gty = pd['Ground Truth Yaw']
-        return gtp,gty
 
 
-def getFiles() -> tuple[str,str]:
-    '''
-        get the files from the predefined path or using tkinter windows
-    '''
-    inFileName:str
-    gtFileName:str
-    if Debug:
-        inFileName:str = inTestFile
-        gtFileName:str = gtTestFile
-    else:
-        root = tkinter.Tk()
-        inFileName = tkinter.filedialog.askopenfilename(initialdir='.', title='Select input CSV file.', filetypes=(('CSV files', '*.csv;*.txt'), ('all files', '*.*')))
-        root.destroy()
-        root = tkinter.Tk()
-        gtFileName = tkinter.filedialog.askopenfilename(initialdir='.', title='Select GroundTruth CSV file.', filetypes=(('CSV files', '*.csv;*.txt'), ('all files', '*.*')))
-        root.destroy()
-    return inFileName, gtFileName
-
-def display(wnd:str, frameWnd:str, window:cv2.typing.MatLike, m:list[str], g:list[str], im_info:str=''):
+def display(wnd:str, frameWnd:str, window:cv2.typing.MatLike, crtVals:list[float], gtVals:list[float], im_info:str=''):
     global crtFrame, pv, N, totalFrames, gty, gtp, pitch, yaw, H
     startingPointInFrame = crtFrame - pv
     nrp = min(N, totalFrames - startingPointInFrame)
@@ -312,54 +285,58 @@ def display(wnd:str, frameWnd:str, window:cv2.typing.MatLike, m:list[str], g:lis
     start_point = (0, frameHeight//2)
     end_point   = (frameWidth, frameHeight//2)
     cv2.line(window, start_point, end_point, (102,102,0), 1)
-    cv2.putText(window, names[crtFrame]+im_info
+    cv2.putText(window, names[crtFrame]+im_info + '    ' + str(ber[crtFrame])
                 ,(0,15),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255))
     for i in range(nrp-1):
         cv2.putText(window, str(startingPointInFrame+i), (i*distBetweenPoints + lrMargin, frameHeight-10), cv2.FONT_HERSHEY_SIMPLEX,  
                    0.3, (102,102,0), 1, cv2.LINE_AA)
 
     # print the points and the lines
-    end_point1=(0,0)
-    end_point2=(0,0)
+    end_point=(0,0)
+    end_point=(0,0)
     for i in range(nrp-1):
-        p1:float= float(m[startingPointInFrame+i])
-        p2:float = float(m[startingPointInFrame+i+1])
-        start_point1 = (i*distBetweenPoints + lrMargin, int(frameHeight//2 - p1*scale))
-        end_point1   = ((i+1)*distBetweenPoints + lrMargin, int(frameHeight//2 - p2*scale))
-        window = cv2.line(window, start_point1, end_point1, color1, thickness1)
-        window = cv2.circle(window,start_point1, radius2, color2, thickness2)
-        p1 = float(g[startingPointInFrame+i])
-        p2 = float(g[startingPointInFrame+i+1])
-        start_point2 = (i*distBetweenPoints + lrMargin, int(frameHeight/2 - p1*scale))
-        end_point2   = ((i+1)*distBetweenPoints + lrMargin, int(frameHeight/2 - p2*scale))
-        window = cv2.line(window, start_point2, end_point2, color3, thickness3)
-        window = cv2.circle(window,start_point2, radius4, color4, thickness4)
+        #print values
+        p1:float= crtVals[startingPointInFrame+i]
+        p2:float = crtVals[startingPointInFrame+i+1]
+        start_point = (i*distBetweenPoints + lrMargin, int(frameHeight//2 - p1*scale))
+        end_point   = ((i+1)*distBetweenPoints + lrMargin, int(frameHeight//2 - p2*scale))
+        window = cv2.line(window, start_point, end_point, color1, thickness1)
+        window = cv2.circle(window,start_point, radius2, color2, thickness2)
+        #print ground truth
+        p1 = gtVals[startingPointInFrame+i]
+        p2 = gtVals[startingPointInFrame+i+1]
+        start_point = (i*distBetweenPoints + lrMargin, int(frameHeight/2 - p1*scale))
+        end_point   = ((i+1)*distBetweenPoints + lrMargin, int(frameHeight/2 - p2*scale))
+        window = cv2.line(window, start_point, end_point, color3, thickness3)
+        colorgt = color4 if ber[startingPointInFrame+i] == 0 else colRed
+        window = cv2.circle(window,start_point, radius4, colorgt, thickness4)
 
-    window = cv2.circle(window,end_point1, radius2, color2, thickness2)
-    window = cv2.circle(window,end_point2, radius4, color4, thickness4)
-    p3 = float(g[crtFrame]) if crtFrame < totalFrames else 0.0
-    crt_point = (pv*distBetweenPoints + lrMargin, int(frameHeight/2 - p3*scale))
+    colorgt = color4 if ber[startingPointInFrame+nrp-1] == 0 else colRed
+    window = cv2.circle(window,end_point, radius2, color2, thickness2)
+    window = cv2.circle(window,end_point, radius4, color4, thickness4)
+    p3 = gtVals[crtFrame] if crtFrame < totalFrames else 0.0
+    crt_point = (pv*distBetweenPoints + lrMargin, int(frameHeight//2 - p3*scale))
     window = cv2.circle(window, crt_point, radius5, color5, thickness5)
     cv2.imshow(wnd, window)
     #framePath = imgDir + 'image_'+str(crtFrame)+'.png'
     framePath = imgDir+names[crtFrame]
 
     image = cv2.imread(framePath) 
-    cv2.putText(image, framePath, (0,15), cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255))
-
-    pt1:cv2.typing.Point=(image.shape[0]//2,image.shape[1]//2)
-
-    leng=50
+    # cv2.putText(image, framePath, (0,15), cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255))
+    if score is not None: cv2.putText(image, str(score[crtFrame]), (0,45), cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255))
+    
     if bd is None:
-        dy:float = leng * (np.sin(float(gtp[crtFrame])) if np.abs(float(gtp[crtFrame]))<np.pi else np.sin(float(gtp[crtFrame]))*np.abs(float(gtp[crtFrame])))
-        dx:float = leng * (np.sin(float(gty[crtFrame])) if np.abs(float(gty[crtFrame]))<np.pi else np.sin(float(gty[crtFrame]))*np.abs(float(gty[crtFrame])))
+        leng=50
+        pt1:cv2.typing.Point=(image.shape[0]//2,image.shape[1]//2)
+        dy:float = leng * (np.sin(gtp[crtFrame]) if np.abs(gtp[crtFrame])<np.pi else np.sin(gtp[crtFrame])*np.abs(gtp[crtFrame]))
+        dx:float = leng * (np.sin(gty[crtFrame]) if np.abs(gty[crtFrame])<np.pi else np.sin(gty[crtFrame])*np.abs(gty[crtFrame]))
         pt2:cv2.typing.Point=np.round((pt1[0]-dy,pt1[1]-dx)).astype(int)
         cv2.arrowedLine(image,pt1, 
                               pt2,
                               color=(0,255,0),thickness=5)
 
-        dy = leng * np.sin(float(pitch[crtFrame]))
-        dx= leng * np.sin(float(yaw[crtFrame]))
+        dy = leng * np.sin(pitch[crtFrame])
+        dx= leng * np.sin(yaw[crtFrame])
         pt2:cv2.typing.Point=np.round((pt1[0]-dy,pt1[1]-dx)).astype(int)
         cv2.arrowedLine(image,pt1, 
                               pt2,
@@ -369,13 +346,13 @@ def display(wnd:str, frameWnd:str, window:cv2.typing.MatLike, m:list[str], g:lis
 
         x.bboxes=np.array([bd[crtFrame]],dtype=np.float32)
 
-        x.pitch=np.array([float(gtp[crtFrame])],dtype=np.float32)
-        x.yaw=np.array([float(gty[crtFrame])],dtype=np.float32)
+        x.pitch=np.array([gtp[crtFrame]],dtype=np.float32)
+        x.yaw=np.array([gty[crtFrame]],dtype=np.float32)
 
         my_pipeline.my_pipeline.my_render(image,x,(0,522,0))
 
-        x.pitch=np.array([float(pitch[crtFrame])],dtype=np.float32)
-        x.yaw=np.array([float(yaw[crtFrame])],dtype=np.float32)
+        x.pitch=np.array([pitch[crtFrame]],dtype=np.float32)
+        x.yaw=np.array([yaw[crtFrame]],dtype=np.float32)
 
         my_pipeline.my_pipeline.my_render(image,x)
 
@@ -387,3 +364,22 @@ def display(wnd:str, frameWnd:str, window:cv2.typing.MatLike, m:list[str], g:lis
 if __name__ == '__main__':
     Debug=True
     main()
+
+
+# def getFiles() -> tuple[str,str]:
+#     '''
+#         get the files from the predefined path or using tkinter windows
+#     '''
+#     inFileName:str
+#     gtFileName:str
+#     if Debug:
+#         inFileName:str = inTestFile
+#         gtFileName:str = gtTestFile
+#     else:
+#         root = tkinter.Tk()
+#         inFileName = tkinter.filedialog.askopenfilename(initialdir='.', title='Select input CSV file.', filetypes=(('CSV files', '*.csv;*.txt'), ('all files', '*.*')))
+#         root.destroy()
+#         root = tkinter.Tk()
+#         gtFileName = tkinter.filedialog.askopenfilename(initialdir='.', title='Select GroundTruth CSV file.', filetypes=(('CSV files', '*.csv;*.txt'), ('all files', '*.*')))
+#         root.destroy()
+#     return inFileName, gtFileName
--- DGA/utilities/Adnotator_app/fileCatUtilities.py
+++ DGA/utilities/Adnotator_app/fileCatUtilities.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 
-base_file = 'D:/DCIM/pitch_and_yaw_b{}.csv'
-gt_file = 'D:/DCIM/gt_b{}.csv'
-out_file = 'D:/DCIM/err_b{}.csv'
-Header = 'Frame,Original Pitch,Ground Truth Pitch,Pitch Err,Original Yaw,Ground Truth Yaw,Yaw Err'
+base_file = 'D:/DCIM/images_12fps/pitch_and_yaw_b{}.csv'
+gt_file = 'D:/DCIM/images_12fps/gt_b{}.csv'
+out_file = 'D:/DCIM/images_12fps/experiment/data/err_b{}.csv'
+Header = 'Frame,Original Pitch,Ground Truth Pitch,Pitch Err,Original Yaw,Ground Truth Yaw,Yaw Err,Frame Err'
 
 def catFile(base:str, gt:str,out:str)->None:
     f = pd.read_csv(base)
@@ -14,17 +14,18 @@ def catFile(base:str, gt:str,out:str)->None:
     y:list[float] = f['Yaw']
     pg:list[float] = g['Ground Truth Pitch']
     yg:list[float] = g['Ground Truth Yaw']
+    be:list[float] = g['Frame Err']
 
     with open(out,'w+') as fileDesc:
         print(Header, file=fileDesc)
         for i in range(len(p)):
-            print(i+1,p[i],pg[i],abs(p[i]-pg[i]),y[i],yg[i],abs(y[i]-yg[i]),
+            print(i+1,p[i],pg[i],abs(p[i]-pg[i]),y[i],yg[i],abs(y[i]-yg[i]), be[i],
                     file = fileDesc,
                     sep=',')
 
 
 def main():
-    for i in range(1,5):
+    for i in range(1,10):
         catFile(base_file.format(i), gt_file.format(i), out_file.format(i))
 
 if __name__=='__main__':
